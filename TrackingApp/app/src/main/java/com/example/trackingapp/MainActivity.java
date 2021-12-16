539742c125dc7621d0a44c825dd3cd3ec015863b
turab45 <turabbajeer202@gmail.com>
2021-12-16 23:55:20 +0500

    added all permissions

--- TrackingApp/app/src/main/java/com/example/trackingapp/MainActivity.java
+++ TrackingApp/app/src/main/java/com/example/trackingapp/MainActivity.java
@@ -2,7 +2,16 @@ package com.example.trackingapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Dialog;
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.GoogleApiAvailability;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -13,10 +22,41 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        if (isServicesOk()){
+            init();
+        }
+    }
+
+    public void init(){
+        Button btnMap = (Button) findViewById(R.id.btnMap);
+        btnMap.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, MapActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 
     public boolean isServicesOk(){
-        
+        Log.d(TAG, "Checking google services version");
+        int available = GoogleApiAvailability.getInstance().isGooglePlayServicesAvailable(MainActivity.this);
+
+        if (available == ConnectionResult.SUCCESS){
+            // everything is fine and user can make map requests
+            Log.d(TAG, "Google play services working...");
+            return true;
+        }else if(GoogleApiAvailability.getInstance().isUserResolvableError(available)){
+            // an error has occurred but we can fix it
+            Log.d(TAG, "isServicesOk: An error occurred but we can fix it.");
+            Dialog dialog = GoogleApiAvailability.getInstance().getErrorDialog(MainActivity.this, available, ERROR_DIALOG_CODE);
+            dialog.show();
+        }else{
+            // you cant make map requests
+            Toast.makeText(this, "You can't make map requests.", Toast.LENGTH_SHORT).show();
+            return false;
+        }
         return  false;
     }
 }
